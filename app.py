826fb09dc963286aa635559a015503f9d5e73350
LFernandez <>
2021-01-05 10:58:11 -0600

    PyLint
    instalacion de pylint

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, abort, request, redirect, url_for
+from modelo.models import db,Usuario
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:hola.123@localhost/WorldCulture'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configuración para el manejo de la sesion de los usuarios
+loginManager = LoginManager()
+loginManager.init_app(app)
+loginManager.login_view = "inicio"
+
+
+@loginManager.user_loader
+def load_user(Id):
+    return Usuario.query.get(int(Id))
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def inicio():
+    if current_user.is_authenticated and current_user.Tipo=="A":
+        return render_template('PanelAdministracion.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    try:
+        u = Usuario()
+        u = u.validar(request.form['inputEmail'], request.form['inputPassword'])
+        if u != None:
+            login_user(u)
+            return redirect(url_for('inicio'))
+        else:
+            return 'Datos No Válidos'
+    except:
+        abort(500)
+    
+@app.route('/cerrarSesion')
+@login_required
+def cerrarSesion():
+    if current_user.is_authenticated:
+        logout_user()
+        return redirect(url_for("inicio"))
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
